2e7a4631cefd364df2538564b444099a15202b36
Evgenii Zakharov <eugene_zaxarov_1997@mail.ru>
2024-02-11 22:22:40 +0300

    rollup

---
+++ index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import MY_IMAGE from "./assets/JS.jpg";
+console.log("Hello World!");
+
+const newText = document.createElement("h1");
+newText.innerText = "I love JavaScript";
+newText.className = "text";
+document.body.append(newText);
+
+const img = document.createElement("img");
+img.src = MY_IMAGE;
+document.body.append(img);
